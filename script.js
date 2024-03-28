29ed250f12f8bcf6d3c3744ffaf70b1160612704
nakbintangjagad <nakbintangjagad@gmail.com>
2024-03-28 13:28:54 +0700

    first

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleUts() {
+    const fp = document.querySelector(".bj-links");
+    const icon = document.querySelector(".bj-icon");
+    fp.classList.toggle("open");
+    icon.classList.toggle("open");
+}
